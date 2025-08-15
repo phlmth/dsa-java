3813b388b2bf0b91098d946e6b6528cc5fe32965
Jose P. <shzexyz@gmail.com>
2025-08-15 10:15:19 -0300

    implements a basic Tree with insert and find operations

---
+++ Tree.java
@@ -0,0 +1,53 @@
+package main.java;
+
+public class Tree {
+    private class Node {
+        private int value;
+        private Node leftChild;
+        private Node rightChild;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node root;
+
+    public void insert(int value) {
+        var node = new Node(value);
+        if (root == null) {
+            root = node;
+            return;
+        }
+
+        var current = root;
+        while (true) {
+            if (value > current.value) {
+                if (current.rightChild == null) {
+                    current.rightChild = node;
+                    break;
+                }
+                current = current.rightChild;
+            }
+            else {
+                if (current.leftChild == null) {
+                    current.leftChild = node;
+                    break;
+                }
+                current = current.leftChild;
+            }
+        }
+    }
+
+    public boolean find(int value) {
+        var current = root;
+        while (current != null) {
+            if (value > current.value)
+                current = current.rightChild;
+            else if (value < current.value)
+                current = current.leftChild;
+            else return true;
+        }
+        return false;
+    }
+}
